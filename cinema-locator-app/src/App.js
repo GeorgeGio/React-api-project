e0bc1807debe956ce6bd1b7a2bd11780878e818e
chari00 <macharitocruzuk@gmail.com>
2023-03-15 16:38:12 +0000

    add navbar.js and navbar.css to navtab components.

--- cinema-locator-app/src/App.js
+++ cinema-locator-app/src/App.js
@@ -1,11 +1,37 @@
+import { useState } from 'react';
 import './App.css';
-import SearchBtn from './components/SearchBtn';
+import SearchBtn from './components/SearchTabs/SearchBtn';
+import Navbar from './components/NavTabs/Navbar';
+
+fetch('https://api-gate2.movieglu.com/filmLiveSearch/?query=max&n=6', {
+  method: 'GET',
+  headers: {
+    "client": "NONE_103",
+    "x-api-key": "	HdSe1JCKF28zruiMLi0KU8RIq8CS0GW65D6zztYb",
+    "authorization": "	Basic Tk9ORV8xMDNfWFg6dXpJYnpWeTNpNEM4=",
+    "territory": "XX",
+    "api-version": "v200",
+    "geolocation": "-22.0;14.0",
+    "device-datetime": "2020-09-14T08:30:17.360Z"
+  },
+})
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+
+
+
+
 
 function App() {
+
+  const [cinema, setCinema] =useState({name:'', location:''})
+
   return (
     <div>
+      <Navbar/>
      <SearchBtn/>
-        
+        {<h1>{cinema.name}</h1>}
     </div>
   );
 }
---
+++ cinema-locator-app/src/components/NavTabs/Navbar.js
@@ -0,0 +1,30 @@
+import '../NavTabs/Navbar.css'
+
+
+
+export default  function Navbar() {
+    
+
+    return<>
+     
+ 
+  <div className="navbar-container" >
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Features</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">New Release</a>
+      </li>
+     
+    </ul>
+  </div>
+
+    
+    </>
+
+
+}
